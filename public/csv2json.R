6553e1e44bb011bd0f1b0d73e3c7679fc3cd6cfd
Martin Turjak <martinturjak@gmail.com>
2015-04-02 17:22:34 +0200

    updated data file

--- public/csv2json.R
+++ public/csv2json.R
@@ -1,4 +1,4 @@
 sp <- read.delim("table.txt", fileEncoding="UTF-8", header=TRUE)
-spdf <- data.frame('species' = sp$Species, 'author' = sp$Author, 'acceptance' = sp$Acceptance, 'family' = sp$Family, 'island' = sp$Island, 'area' = sp$Area, 'origin' = sp$Origin, 'invasive_capacity' = sp$Invasive.capacity, 'plot' = sp$Plot, 'habitus' = sp$habitus, 'lifeform' = sp$life.form, 'collected' = sp$collected, 'photo' = sp$photo, 'fruit' = sp$Fruit.type, 'comments' = sp$comments)
+spdf <- data.frame('species' = sp$Species, 'author' = sp$Author, 'family' = sp$Family, 'habitat' = sp$Area, 'origin' = sp$Origin, 'invasive_capacity' = sp$Invasive.capacity, 'habit' = sp$habitus, 'lifeform' = sp$life.form, 'fruit_type' = sp$Fruit.type, 'comments' = sp$comments)
 jsn <- toJSON(spdf)
 jsn
