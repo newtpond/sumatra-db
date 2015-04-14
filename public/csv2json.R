79d2a66a2fbff19b14c5bb8afad3cff3e436cb5c
Martin Turjak <martinturjak@gmail.com>
2015-04-14 22:37:01 +0200

    jsonlite library to R script

--- public/csv2json.R
+++ public/csv2json.R
@@ -1,3 +1,4 @@
+library('jsonlite')
 sp <- read.delim("table.txt", fileEncoding="UTF-8", header=TRUE)
 spdf <- data.frame('species' = sp$Species, 'author' = sp$Author, 'family' = sp$Family, 'habitat' = sp$Area, 'origin' = sp$Origin, 'invasive_capacity' = sp$Invasive.capacity, 'habit' = sp$habitus, 'lifeform' = sp$life.form, 'fruit_type' = sp$Fruit.type, 'comments' = sp$comments)
 jsn <- toJSON(spdf)
